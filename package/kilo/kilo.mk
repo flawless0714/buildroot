1ddcc0d08d55ec3f12d1438c2df060c1070a3c09
flawless0714 <flawless0714@gmail.com>
2019-04-06 11:23:19 +0800

    Fix .mk file for package `kilo`
    We should use fetching method provided by Buildroot to get package
    at GitHub. The `make` command is also faulty, which a target
    should be specified.

--- package/kilo/kilo.mk
+++ package/kilo/kilo.mk
@@ -4,17 +4,17 @@
 #
 ################################################################################
 
-KILO_SITE = https://github.com/sysprog21/kilo.git
-KILO_SITE_METHOD = git
+KILO_VERSION=af3919d68cb2e70a3d9a2309596cf290cf6bc1ac
+KILO_SITE = $(call github,sysprog21,kilo,$(KILO_VERSION))
 KILO_LICENSE = BSD 2-clause License
 
 define KILO_BUILD_CMDS
-	$(MAKE) CC="$(TARGET_CC)" CFLAGS+="$(TARGET_CFLAGS)" \
-		LIBS="$(KILO_EXTRA_LIBS)"
+	$(MAKE) CC="$(TARGET_CC)" CFLAGS+="$(TARGET_CFLAGS)" -C \
+		$(@D) all
 endef
 
 define KILO_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0775 -D $(@D)/nano $(HOST_DIR)/usr/bin/nano
+	$(INSTALL) -m 0775 -D $(@D)/kilo $(TARGET_DIR)/usr/bin/kilo
 endef
 
 $(eval $(generic-package))
